71f62aaaeed370ebdd6b2102fb2c2f9acc9a8534
Mwenda Muriithi <mwenda.muriithi@strathmore.edu>
2026-01-29 00:09:30 +0300

    feat: frontend authentication module

--- car-recommender/src/app/app.routes.ts
+++ car-recommender/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './features/auth/auth';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'auth',
+        component: AuthComponent
+    }
+];
