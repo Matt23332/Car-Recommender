dcb1858bad414909205dc953f89f9f8acc4327a0
Mwenda Muriithi <mwenda.muriithi@strathmore.edu>
2026-01-29 03:41:47 +0300

    feat: designing the dashboard/landing page

--- car-recommender/src/app/app.routes.ts
+++ car-recommender/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './features/auth/auth';
+import { DashboardComponent } from './features/dashboard/dashboard';
 
 export const routes: Routes = [
     {
         path: 'auth',
         component: AuthComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
     }
 ];
